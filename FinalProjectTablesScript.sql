cf12d8e8d2192960a1ce0117f71ffd475f5eaa82
Gregory Pugh <arbitwah@gmail.com>
2018-07-18 17:38:07 -0500

    Nearly Finished Script one minor error with date, fixed if statments, added a test insert script.

--- FinalProjectTablesScript.sql
+++ FinalProjectTablesScript.sql
@@ -1,11 +1,11 @@
-USE master;
+USE gp0541031;
 GO
 
-IF OBJECT_ID ('Student') IS NOT NULL
-DROP TABLE Student;
+IF OBJECT_ID ('Students') IS NOT NULL
+DROP TABLE Students;
 
-IF OBJECT_ID('Instructor') IS NOT NULL
-DROP TABLE Instructor;
+IF OBJECT_ID('Faculty') IS NOT NULL
+DROP TABLE Faculty;
 
 IF OBJECT_ID('Department') IS NOT NULL
 DROP TABLE Department;
@@ -21,35 +21,44 @@ USE gp0541031;
  StudentID INT NOT NULL PRIMARY KEY IDENTITY,
  FirstName VARCHAR(20) NOT NULL,
  LastName VARCHAR(20) NOT NULL,
- BirthDate DATE NOT NULL,
- Gender VARCHAR(6) NOT NULL, 
- PhoneNumber VARCHAR(20) NOT NULL,
+ BirthDate DATE NOT NULL DEFAULT '1/1/1901',
+ Gender VARCHAR(1) NOT NULL , 
+ PhoneNumber VARCHAR(12) NOT NULL,
  GPA INT NOT NULL,
- StateAbb VARCHAR(2) NOT NULL);
+ State VARCHAR(2) NOT NULL, 
+ CONSTRAINT CK_StateAbbr CHECK (State LIKE '[A-Z][A-Z]'),
+ CONSTRAINT CK_MaleFemale CHECK (Gender = 'M' OR Gender = 'F'),
+ CONSTRAINT CK_PhoneNumber CHECK (PhoneNumber LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'));
  
- CREATE TABLE Instructors(
- InstructorID INT NOT NULL PRIMARY KEY IDENTITY,
+ CREATE TABLE Faculty(
+ FacultyID INT NOT NULL PRIMARY KEY IDENTITY,
  FirstName VARCHAR(20) NOT NULL,
  LastName VARCHAR(20) NOT NULL,
  HeldDegree VARCHAR(40) NOT NULL,
- ShcoolAttended VARCHAR(50) NOT NULL);
+ ShcoolAttended VARCHAR(50) NOT NULL,
+ Title VARCHAR(21) NOT NULL);
  
  CREATE TABLE Department(
  DeptID INT NOT NULL PRIMARY KEY IDENTITY,
- ProgramCode INT NOT NUll,
- Dean VARCHAR(41) NOT NULL,
- Division VARCHAR(30) NOT NULL);
+ ProgramCode VARCHAR(3) NOT NUll,
+ Division VARCHAR(30) NOT NULL,
+ CONSTRAINT CK_ProgramCd CHECK (ProgramCode LIKE '[A-Z][A-Z][A-Z]')) ;
  
  CREATE TABLE Course(
  CourseID INT NOT NULL PRIMARY KEY IDENTITY,
  StudentID INT NOT NULL REFERENCES Students(StudentID),
- InstructorID INT NOT NULL REFERENCES Instructors(InstructorID),
+ FacultyID INT NOT NULL REFERENCES Faculty(FacultyID),
  DeptID INT NOT NULL REFERENCES Department(DeptID),
- CreditHours INT NOT NULL,
- DaysOffered DATE NOT NULL,
+ CreditHours INT NOT NULL CHECK (CreditHours >= 0),
+ DaysOffered VARCHAR(6) NOT NULL,
  Semester VARCHAR(6) NOT NULL,
  RoomNumber VARCHAR(10) NOT NULL,
  SectionNumber VARCHAR(10) NOT NULL,
  CourseTitle VARCHAR(30) NOT NULL,
- Subject VARCHAR(30) NOT NULL);
-  
+ Subject VARCHAR(30) NOT NULL,
+ CONSTRAINT CK_CreditHours CHECK (CreditHours >= 0));
+
+ CREATE INDEX IX_StudentLastName ON Students(LastName);
+ CREATE INDEX IX_StudentHomePhone ON Students(PhoneNumber);
+
+
---
+++ Test Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO Students
+VALUES('Gregory','Pugh','7/11/1992','M','417-239-9222',3,'MO')
+
+INSERT INTO Faculty
+VALUES('Tiffany','Ford','Masters','Drury','Instructor') 
+
+INSERT INTO Department
+VALUES('CIS','Technology')
+
+INSERT INTO Course
+VALUES(1,1,1,3,'Mon','Spring','NKM204','101','Web Development','HTML')
